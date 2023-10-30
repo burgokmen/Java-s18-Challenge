902fa7aba37d4c5abd653f0dda98e03fa44a65ef
burgokmen <burgokmen@gmail.com>
2023-10-30 04:42:40 +0300

    init

---
+++ src/main/java/com/brutech/library_rest_api/LibraryRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.brutech.library_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryRestApiApplication.class, args);
+    }
+
+}
